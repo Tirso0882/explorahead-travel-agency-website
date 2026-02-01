31f851ce01784a4019ce45fe75b8f7d5d0ada443
Tirso Gomez <tirso.gomez.ml@gmail.com>
2026-02-01 16:55:44 +0100

    refactor: update root app files and metadata configuration

--- app/robots.ts
+++ app/robots.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2024-2026 ExplorAhead. All rights reserved.
+ * This file is part of proprietary software. See LICENSE for terms.
+ */
+
 import { MetadataRoute } from "next";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://explorahead.com";
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2024-2026 ExplorAhead. All rights reserved.
+ * This file is part of proprietary software. See LICENSE for terms.
+ */
+
 import { getPublishedPages } from "@/config/pages";
 import { MetadataRoute } from "next";
 
